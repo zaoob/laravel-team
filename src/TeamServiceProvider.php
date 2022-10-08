55282c8a8728b13a689d1cf43acfdb4222cac95f
Karim Mohamed <komicho1996@gmail.com>
2022-10-08 12:40:01 +0200

    first commit

---
+++ src/TeamServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Zaoob\Laravel\Team;
+
+use Illuminate\Support\ServiceProvider;
+
+class TeamServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
+
+
+
