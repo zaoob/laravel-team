12e62a5031743971ef5325f6ae5dc77d55d89506
Karim Mohamed <komicho1996@gmail.com>
2022-10-08 15:16:19 +0200

    Check member exists before adding to the team

--- src/Traits/Teamable.php
+++ src/Traits/Teamable.php
@@ -8,13 +8,24 @@ trait Teamable
 {
     public function addMember($user_id, $rule = null, $expires_at = null)
     {
-        return $this->morphOne(Team::class, 'modelable')
-            ->create([
-                'member_type' => config('auth.providers.users.model'),
-                'member_id' => $user_id,
-                'rule' => $rule,
-                'expires_at' => $expires_at,
-            ]);
+        $users_model = config('auth.providers.users.model');
+
+        $member = $this->morphOne(Team::class, 'modelable')
+            ->where('member_type', $users_model)
+            ->where('member_id', $user_id)
+            ->first();
+
+        if (!$member) {
+            $member = $this->morphOne(Team::class, 'modelable')
+                ->create([
+                    'member_type' => $users_model,
+                    'member_id' => $user_id,
+                    'rule' => $rule,
+                    'expires_at' => $expires_at,
+                ]);
+        }
+
+        return $member;
     }
 
     public function getMember($user_id)
